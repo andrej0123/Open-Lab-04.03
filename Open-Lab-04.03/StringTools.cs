5df016cbc2299f0cc0cf81656196a55623b6428a
z20ist24 <z20ist24@Z00P1-05>
2021-11-05 10:07:07 +0100

    removes the first and last letter from a string

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            if (original.Length >= 2)
+            {
+                string notzero = original.Remove(0, 1);
+                string result = notzero.Remove(notzero.Length - 1);
+                return result;
+            }
+            else 
+            {
+                return original;
+            }
+           
         }
     }
 }
